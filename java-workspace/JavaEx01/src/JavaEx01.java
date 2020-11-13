90f24b0e93b9ea227f7b96b68f592a4dc9b8c58e
ParkDahye <clo_udhye@naver.com>
2020-11-13 17:22:04 +0900

    Make backup java-workspace Folder

---
+++ java-workspace/JavaEx01/src/JavaEx01.java
@@ -0,0 +1,12 @@
+//패키지 선언이 없으면 디폴트 패키지
+public class JavaEx01 {
+
+	
+	  public static void main(String[] args) { // TODO Auto-generated method stub
+	  System.out.println("디폴트 패키지aaaff "); 
+	  System.out.println();
+	  
+	  }
+	 
+
+}
---
+++ java-workspace/JavaEx01/src/com/JavaEx01.java
@@ -0,0 +1,10 @@
+package com;
+
+public class JavaEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("패키지");
+	}
+
+}
