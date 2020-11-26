9df54ff5045d657ff30b26136a4cae31fd41e748
ParkDahye <clo_udhye@naver.com>
2020-11-26 13:02:17 +0900

    java 2020-11-26 [Thread] Thread Example (join)

---
+++ java-workspace/ThreadEx01/src/JoinExample.java
@@ -0,0 +1,18 @@
+
+public class JoinExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//p. 607예제 다른 스레드가 종료될 때까지 일시정지 상태 유지
+		
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		
+		try {
+			sumThread.join();	//sumThread가 종료될 때까지 메인 스레드를 종료시킴
+		} catch (InterruptedException e) {}
+		
+		System.out.println("1~100까지 합 : " + sumThread.getSum());
+	}
+
+}
