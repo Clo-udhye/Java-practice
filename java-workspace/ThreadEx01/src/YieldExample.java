641d579e79aa6041bbc03ae9448bcf058581288b
ParkDahye <clo_udhye@naver.com>
2020-11-26 13:01:52 +0900

    java 2020-11-26 [Thread] Thread Example (yield)

---
+++ java-workspace/ThreadEx01/src/YieldExample.java
@@ -0,0 +1,33 @@
+
+public class YieldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// p.604 예제 스레드 실행 양보 예제
+		
+		ThreadA threadA = new ThreadA();
+		ThreadB threadB = new ThreadB();
+		
+		threadA.start();
+		threadB.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {}
+		threadA.work = false;
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {}
+		threadA.work = true;
+		
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {}
+		
+		threadA.stop = true;
+		threadB.stop = true;
+	}
+
+}
