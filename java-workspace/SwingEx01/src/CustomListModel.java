01a8b0b19aa45e89a18f5f795c0377b70f577347
ParkDahye <clo_udhye@naver.com>
2020-12-10 14:37:00 +0900

    java 2020-12-10 [Swing] JList Example using WindowBuilder

--- java-workspace/SwingEx01/src/CustomListModel.java
+++ java-workspace/SwingEx01/src/CustomListModel.java
@@ -3,6 +3,16 @@ import javax.swing.AbstractListModel;
 public class CustomListModel extends AbstractListModel<String> {
 	private String[] values = new String[] {"사과", "딸기", "수박", "참외"};
 	
+	/*
+	public CustomListModel() {
+		// TODO Auto-generated constructor stub
+		values.add("사과");
+		values.add("딸기");
+		values.add("수박");
+		values.add("참외");
+	}
+	 */
+	
 	@Override
 	public String getElementAt(int index) {
 		// TODO Auto-generated method stub
