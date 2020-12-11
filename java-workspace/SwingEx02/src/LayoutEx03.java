004bf4402da1342282751cf395e325a0cbda4712
ParkDahye <clo_udhye@naver.com>
2020-12-11 15:43:05 +0900

    java 2020-12-11 [Swing] Layout Example GridLayout

---
+++ java-workspace/SwingEx02/src/LayoutEx03.java
@@ -0,0 +1,59 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+public class LayoutEx03 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LayoutEx03 frame = new LayoutEx03();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LayoutEx03() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(17, 15, 394, 229);
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(2, 2, 5, 5));
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton);
+	}
+
+}
