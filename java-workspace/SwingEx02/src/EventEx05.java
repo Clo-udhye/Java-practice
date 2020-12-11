f35aebf5dd10db38e9f0cd38ec944ddf84a1df5e
ParkDahye <clo_udhye@naver.com>
2020-12-11 21:51:06 +0900

    java 2020-12-11 [Swing] Event Example External class

---
+++ java-workspace/SwingEx02/src/EventEx05.java
@@ -0,0 +1,47 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class EventEx05 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EventEx05 frame = new EventEx05();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EventEx05() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btn = new JButton("New button");
+		btn.setBounds(17, 15, 129, 29);
+		btn.addMouseListener(new UserMouseListener());
+		
+		contentPane.add(btn);
+	}
+
+}
